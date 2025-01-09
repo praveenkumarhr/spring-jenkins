def1032a2c2c5258ed7e8718e09ed0019ebb45c4
praveenkumarhr <hrkpravee@gmail.com>
2025-01-09 10:47:55 +0530

    first commit

---
+++ src/test/java/com/learning/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
